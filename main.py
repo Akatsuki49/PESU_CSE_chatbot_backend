cd5ec8021b4a319d5788a7a84c5662dbe088c5fb
Sowmesh <PES1202100690@pesu.pes.edu>
2025-02-09 00:46:15 +0530

    ability to delete stuff as well

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from store import store_xl, store_single_qa
 from websocketManager import ConnectionManager
 from getSimilarQuestions import getSimilarQuestions
 import json
+from delete_points import delete_points
 
 app = FastAPI()
 manager = ConnectionManager()
@@ -34,52 +35,58 @@ async def upload_data(file: UploadFile):
             raise HTTPException(status_code=500, detail=str(e))
     else:
         raise HTTPException(status_code=400, detail="Invalid file type. Only .xlsx files are supported.")
-    
-# @app.post("/upload_single_qa/")
-# async def upload_single_qa(question: str, answer: str):
-#     try:
-#         store_single_qa(question, answer)
-#         return {"message": "Question and answer stored successfully."}
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
-@app.websocket("/communicate")
-async def websocket_endpoint(websocket: WebSocket):
-    await manager.connect(websocket)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"Received:{data}",websocket)
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
 
 @app.websocket("/upload_single_qa/")
 async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
+    isDelete_thread = False
+    gotQnA = False
     try:
         while True:
             data = await websocket.receive_json()
-            if "question" in data and "answer" in data:
-                question = data["question"]
-                answer = data["answer"]
-                similar_data = getSimilarQuestions(question)
-                similar_qna = []
-                for data in similar_data:
-                    qna = {
-                            "question": data.payload["question"],
-                            "answer": data.payload["answer"]
-                        }
-                    similar_qna.append(qna)
-                
-                qna_json = json.dumps(similar_qna)
-                await manager.send_personal_message(f"Similar questions and answers:",websocket)
-                await manager.send_personal_message(qna_json,websocket)
-            else:
-                if "confirm" in data:
-                    store_single_qa(question, answer)
-                    await manager.send_personal_message(f"Data stored successfully",websocket)
+            try:
+                if "question" in data and "answer" in data:
+                    gotQnA = True
+                    question = data["question"]
+                    answer = data["answer"]
+                    similar_data = getSimilarQuestions(question)
+                    similar_qna = []
+                    for data in similar_data:
+                        qna = {
+                                "id": data.id,
+                                "question": data.payload["question"],
+                                "answer": data.payload["answer"]
+                            }
+                        similar_qna.append(qna)
+                    
+                    qna_json = json.dumps(similar_qna)
+                    await manager.send_personal_message(f"Similar questions and answers:",websocket)
+                    await manager.send_personal_message(qna_json,websocket)
+                elif "delete" in data:
+                    await manager.send_personal_message(f"Send IDs to delete:",websocket)
+                    isDelete_thread = True
+                elif isDelete_thread:
+                    if "ids" in data:
+                        ids = data["ids"]
+                        delete_points(ids)
+                        isDelete_thread = False
+                        await manager.send_personal_message(f"Data deleted successfully;",websocket)
+                elif "confirm" in data:
+                    if not gotQnA:
+                        await manager.send_personal_message(f"No data uploaded",websocket)
+                    else:
+                        store_single_qa(question, answer)
+                        gotQnA = False
+                        await manager.send_personal_message(f"Data stored successfully",websocket)
+                        manager.disconnect(websocket)
+                elif "abort" in data:
+                        await manager.send_personal_message(f"No data uploaded",websocket)
+                        manager.disconnect(websocket)
+            except Exception as e:
+                await manager.send_personal_message(f"An error occurred: {str(e)}",websocket)
+                isDelete_thread = False
+                gotQnA = False
                 manager.disconnect(websocket)
-
 
     except WebSocketDisconnect:
         manager.disconnect(websocket)
